a485211a87a9cb3cf332412745be89291eab4356
Zack OConnor <zackary25@gmail.com>
2025-02-25 16:46:58 -0500

    Added a way to earn and keep track of gold collected.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const prompt = require('prompt-sync')();
-const playerName = prompt("Welcome to the mystical forest! What is your name?");
+let playerName = prompt("Welcome to the mystical forest! What is your name?");
+let gold = 10;
 
 function startGame() {
     
     const choice1 = prompt(`Hello, ${playerName}! You find yourself in a mystical forest. Do you choose the dark cave or the winding river? (cave/river)`);
   
     if (choice1 === "cave") {
-      cavePath();
+        cavePath();
     } else if (choice1 === "river") {
-      riverPath();
+        riverPath();
     } else {
-      console.log("Invalid choice, please try again.");
-      startGame();
+        invalidChoiceDialog();
+        startGame();
     }
   }
   
@@ -19,13 +20,16 @@ function startGame() {
     const choice2 = prompt("You encounter a gentle creature. Do you talk to it or try to sneak past? (talk/sneak)");
     
     if (choice2 === "talk") {
-        console.log(`The creature guides you to the treasure, ${playerName}. You win!`);
+        let goldFoundInCave = 10;
+        console.log(`The creature guides you to some hidden treasure. You find ${goldFoundInCave} gold pieces.`);
+        gold += goldFoundInCave
+        console.log(`You now have ${gold} pieces of gold!`);
     } else if (choice2 === "sneak") {
-        console.log("You get lost in the cave. Try again.");
-      cavePath();
+        console.log("You get lost in the cave and find nothing. Try again.");
+        cavePath();
     } else {
-        console.log("Invalid choice, please try again.");
-      cavePath();
+        invalidChoiceDialog();
+        cavePath();
     }
   }
   
@@ -33,18 +37,20 @@ function startGame() {
     const choice2 = prompt("You find a rickety bridge. Do you cross it or search for another way? (cross/search)");
     
     if (choice2 === "cross") {
-        console.log(`You safely cross and find a clue for another adventure, ${playerName}. You win!`);
+        let goldFoundInForest = 5;
+        console.log(`You safely crossed the river and found a hidden pouch with ${goldFoundInForest} gold pieces!`);
+        gold += goldFoundInForest;
+        console.log(`You have ${goldFoundInForest} gold pieces!`);
     } else if (choice2 === "search") {
         console.log("You find a safer path but lose time. Try again.");
-      riverPath();
+        riverPath();
     } else {
-      console.log("Invalid choice, please try again.");
-      riverPath();
+        invalidChoiceDialog();
+        riverPath();
     }
   }
+
+
+  invalidChoiceDialog = () => (console.log("Invalid choice, please try again."));
   
   startGame();
-
-
-//   random amoun of money 
-//   allow user to travel and buy gear
